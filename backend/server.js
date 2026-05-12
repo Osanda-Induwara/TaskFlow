750010bbb62b3fcfb3d136231723891d5665aee4
Osanda-Induwara <osanda2002@gmail.com>
2026-05-12 14:57:59 +0530

    Set up Docker for frontend and backend services. Fixed minor bugs

--- backend/server.js
+++ backend/server.js
@@ -90,6 +90,7 @@ function broadcast(boardId, message) {
   }
 }
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
